21aac098b5e6c38fb2ba964dc6bd3d5b891863ea
Penguin-Killer <84609670+Penguin-Killer@users.noreply.github.com>
2024-07-20 14:36:14 +0800

    Update Add 1.1x speed to bilibili.js

--- Add 1.1x speed to bilibili.js
+++ Add 1.1x speed to bilibili.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         b站增加1.1倍速
 // @namespace    https://github.com/Penguin-Killer
-// @version      0.2.5
+// @version      0.3.0
 // @description  将0.75倍速修改为1.1倍速
 // @author       Penguin-Killer
 // @match        *://*.bilibili.com/video/*
@@ -10,33 +10,42 @@
 // @grant        none
 // @license      MIT
 
+// @downloadURL https://update.greasyfork.org/scripts/494155/b%E7%AB%99%E5%A2%9E%E5%8A%A011%E5%80%8D%E9%80%9F.user.js
+// @updateURL https://update.greasyfork.org/scripts/494155/b%E7%AB%99%E5%A2%9E%E5%8A%A011%E5%80%8D%E9%80%9F.meta.js
 // ==/UserScript==
 
 /*
 ***********************************************************
-如果不想自动使用1.1倍速，可以将第30行 “element.click();” 注释掉
+如果不想自动使用1.1倍速，可以将第33行 “element.click();” 注释掉
 ***********************************************************
 */
 
 (function() {
     'use strict';
 
-    function modifyPlaybackSpeedOption() {
-        var element = document.querySelector('.bpx-player-ctrl-playbackrate-menu-item[data-value="0.75"]');
-        if (element) {
-            element.textContent = '1.1x';
-            element.setAttribute('data-value', '1.1');
-            console.log('1.1倍速修改成功！');
-            // 触发点击事件
-            element.click();
-        } else {
-            console.log('未找到要修改的元素。');
-        }
+    function checkAndModifyElement() {
+    var element = document.querySelector('.bpx-player-ctrl-playbackrate-menu-item[data-value="0.75"]');
+    if (element) {
+        element.textContent = '1.1x';
+        element.setAttribute('data-value', '1.1');
+        console.log('1.1倍速修改成功！任务退出');
+        // 触发点击事件
+        element.click();
+        return true; // 找到并修改元素，返回 true 以退出循环
+    } else {
+        console.log('未找到要修改的元素。');
+        return false; // 未找到元素，返回 false 继续循环
     }
+}
 
-    // 在页面加载完成后再运行脚本
-    window.addEventListener('load', function() {
-        // 添加延迟以确保页面完全加载
-        setTimeout(modifyPlaybackSpeedOption, 2000); // 这里设置了2秒延迟，你可以根据需要调整
-    });
+function startChecking() {
+    let intervalId = setInterval(function() {
+        if (checkAndModifyElement()) {
+            clearInterval(intervalId); // 找到并修改元素后清除定时器
+        }
+    }, 5000); // 每隔5秒检测一次
+}
+
+startChecking();
+
 })();
